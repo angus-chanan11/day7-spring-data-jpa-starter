cbcdb8c69f3c04c2fb615042d79079b33b41692f
angus-chanan11 <angus.chan@oocl.com>
2024-11-26 21:22:54 +0800

    refactor: remove unused import

--- src/main/java/com/oocl/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/oocl/springbootemployee/service/EmployeeService.java
@@ -5,13 +5,12 @@ import com.oocl.springbootemployee.exception.EmployeeAgeSalaryNotMatchedExceptio
 import com.oocl.springbootemployee.exception.EmployeeInactiveException;
 import com.oocl.springbootemployee.model.Employee;
 import com.oocl.springbootemployee.model.Gender;
-import com.oocl.springbootemployee.repository.EmployeeInMemoryRepository;
-import java.util.List;
-
 import com.oocl.springbootemployee.repository.EmployeeRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
